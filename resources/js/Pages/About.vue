0af56167471c79be8cfc4b6bfd69d7383ed34579
dreadnought2099 <magallanesraymart@gmail.com>
2025-04-15 16:41:41 +0800

    first commit

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { Head } from "@inertiajs/vue3";
+import PageLayout from "./PageLayout.vue";
+</script>
+
+<template>
+    <Head title="About Page" />
+
+    <PageLayout>
+        <div
+            class="bg-green-100 border border-green-300 p-8 rounded-lg w-[600px] mx-auto mt-12"
+        >
+            <div class="text-center">
+                <p><strong>Name: </strong>Raymart E. Magallanes</p>
+                <p><strong>Course & Year: </strong>BSIT 3</p>
+                <p><strong>Cell No. : </strong>0950 665 3665</p>
+            </div>
+        </div>
+    </PageLayout>
+</template>
---
+++ resources/js/Pages/Investor/Index.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { Head, Link } from "@inertiajs/vue3";
+import PageLayout from "../PageLayout.vue";
+
+const props = defineProps({
+    investors: Object,
+});
+</script>
+
+<template>
+    <Head title="Investors" />
+
+    <PageLayout>
+        <div class="px-6 py-6">
+            <div class="flex justify-between items-center mb-4 mt-4">
+                <h1 class="text-2xl font-bold text-white">Investors</h1>
+                <Link
+                    href="/investors/create"
+                    class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-white hover:text-blue-600 border-1 hover:border-blue-600 transition-all ease-in-out duration-300"
+                >
+                    Add Investor
+                </Link>
+            </div>
+            <div
+                class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
+            >
+                <div
+                    v-for="investor in props.investors.data"
+                    :key="investor.id"
+                    class="bg-green-100 border border-green-300 p-2 rounded-md text-sm max-w-xs"
+                >
+                    <h2 class="text-lg font-semibold mb-2">
+                        {{ investor.last_name }}, {{ investor.first_name }}
+                    </h2>
+                    <p><strong>Phone:</strong> {{ investor.phone }}</p>
+                    <p><strong>Address:</strong> {{ investor.address }}</p>
+                    <p>
+                        <strong>Investment Type:</strong>
+                        {{ investor.investment_type }}
+                    </p>
+                </div>
+            </div>
+
+            <div class="mt-4 flex space-x-2 justify-between">
+                <Link
+                    v-if="props.investors.prev_page_url"
+                    :href="props.investors.prev_page_url"
+                    class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-white hover:text-blue-600 border-1 hover:border-blue-600 transition-all ease-in-out duration-300"
+                >
+                    ← Previous
+                </Link>
+                <Link
+                    v-if="props.investors.next_page_url"
+                    :href="props.investors.next_page_url"
+                    class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-white hover:text-blue-600 border-1 hover:border-blue-600 transition-all ease-in-out duration-300"
+                >
+                    Next →
+                </Link>
+            </div>
+        </div>
+    </PageLayout>
+</template>
